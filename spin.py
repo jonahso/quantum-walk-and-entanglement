7a558d6ec1fd60d71e85196ad39e7fdbfe5647ec
Jue Xu <xujuexujue@hotmail.com>
2025-09-01 18:22:26 +0800

    add power-law Hamiltonian

--- spin.py
+++ spin.py
@@ -57,3 +57,33 @@ class Nearest_Neighbour_1d:
     # def lc_group(self, right, left, step):
     #     self.ham_lc = []
 
+
+
+
+class Power_Law:
+    def __init__(self, n: int, alpha: int, Jx=0, Jy=0, Jz=0, hx=0.0, hy=0.0, hz=0, pbc=False, verbose=False):
+        self.n, self.alpha = n, alpha
+        self.Jx, self.Jy, self.Jz = Jx, Jy, Jz
+        self.hx, self.hy, self.hz = hx, hy, hz
+        self.xx_tuples = [('XX', [i, j], Jx*abs(i-j)**(-alpha)) for i in range(0, n-1) for j in range(i+1, n)]
+        self.yy_tuples = [('YY', [i, j], Jy*abs(i-j)**(-alpha)) for i in range(0, n-1) for j in range(i+1, n)]
+        self.zz_tuples = [('ZZ', [i, j], Jz*abs(i-j)**(-alpha)) for i in range(0, n-1) for j in range(i+1, n)]
+        self.x_tuples = [('X', [i], hx) for i in range(0, n)] 
+        self.y_tuples = [('Y', [i], hy) for i in range(0, n)] 
+        self.z_tuples = [('Z', [i], hz) for i in range(0, n)] 
+        if pbc: 
+            # self.xx_tuples.append(('XX', [n-1, 0], Jx))
+            # self.yy_tuples.append(('YY', [n-1, 0], Jy))
+            # self.zz_tuples.append(('ZZ', [n-1, 0], Jz))
+            raise ValueError(f'PBC is not defined!')
+
+        self.ham = SparsePauliOp.from_sparse_list([*self.xx_tuples, *self.yy_tuples, *self.zz_tuples, *self.x_tuples, *self.y_tuples, *self.z_tuples], num_qubits=n).simplify()
+        if verbose: print('The Hamiltonian: \n', self.ham)
+        self.xyz_group()
+
+    def xyz_group(self):
+        self.x_terms = SparsePauliOp.from_sparse_list([*self.xx_tuples, *self.x_tuples], self.n).simplify()
+        self.y_terms = SparsePauliOp.from_sparse_list([*self.yy_tuples, *self.y_tuples], self.n).simplify()
+        self.z_terms = SparsePauliOp.from_sparse_list([*self.zz_tuples, *self.z_tuples], self.n).simplify()
+        self.ham_xyz = [self.x_terms, self.y_terms, self.z_terms]
+        self.ham_xyz = [item for item in self.ham_xyz if not np.all(abs(item.coeffs) == 0)]
